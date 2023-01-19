f6bcce2cb3414ab8e76ee2a5ff3d2d2d758e6ad6
Fadila-Ali <fadilaali@pursuit.org>
2023-01-19 10:33:53 -0500

    part 2 done

--- controllers/songController.js
+++ controllers/songController.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const songs = express.Router();
-const { getAllSongs } = require("../queries/songs");
+const {
+  checkBoolean,
+  checkName,
+  checkArtist,
+} = require("../validations/checkSongs");
+const { getAllSongs, getSong, createSong } = require("../queries/songs");
 
 // INDEX
 songs.get("/", async (req, res) => {
@@ -9,6 +14,27 @@ songs.get("/", async (req, res) => {
     res.status(200).json(allSongs);
   } else {
     res.status(500).json({ error: "server error" });
+  }
+});
+
+// SHOW ONE SONG
+songs.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const song = await getSong(id);
+  if (!song.message) {
+    res.json(song);
+  } else {
+    res.status(404).json({ error: "not found" });
+  }
+});
+
+// CREATE NEW SONG
+songs.post("/", checkBoolean, checkName, checkArtist, async (req, res) => {
+  try {
+    const song = await createSong(req.body);
+    res.json(song);
+  } catch (error) {
+    res.status(400).json({ error: error });
   }
 });
 
